5ca46cfa7c2855a499105b46b9f5e2d09c9eb4c3
Nikita Sheremet <nikita.sheremet253@gmail.com>
2021-04-07 06:41:48 -0400

    add simple form to login, add logic to check for token and send it along with the request, add logic to save token to local storage after successful login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [formData, setFormData] = useState({username: '', password: ''})
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const token = localStorage.getItem('closetlyToken')
+    const {username, password} = formData
+    const loginResult = await axios.post("http://localhost:3000/login", {
+      username, password, token
+    })
+    if (loginResult.data.createdToken) {
+      localStorage.setItem('closetlyToken', JSON.stringify(loginResult.data.createdToken))
+    }
+    if (loginResult.data.loggedIn) {
+      console.log("Redirect the user please!!")
+    }
+  }
+  
+  const updateData = (updateEvent: React.FormEvent<HTMLInputElement>): void => {
+    const {currentTarget: {name: formKey, value}} = updateEvent
+    setFormData(formData => ({...formData, [formKey]: value}))
+  }
+ return (
+   <form style={{margin: 10}} onSubmit={handleSubmit} action="http://localhost:3000/login" method="post"> 
+     <label htmlFor="username">Username</label>
+     <input id="username" name="username" type="text" value={formData.username} onChange={updateData}/>
+     <label htmlFor="password">Password</label>
+     <input id="password" name="password" type="password" value={formData.password} onChange={updateData}/>
+     <button type="submit">Submit</button>
+   </form>
+ )
 }
 
 export default App;
