8b6019466fc3ad7e33138c354c537986366e57ef
Nikita Sheremet <nikita.sheremet253@gmail.com>
2021-08-01 16:12:53 -0400

    expand image details component, and move delete functionality from the image card to the image details modal

--- src/components/Home/ImageCard.tsx
+++ src/components/Home/ImageCard.tsx
@@ -1,27 +1,12 @@
-import axios from "axios"
-import {
-    useHistory
-  } from "react-router-dom";
 import "./css/ImageCard.css"
 function ImageCard({imageDetails, togglIsImageDetailsShown, setImageDetails}) {
-    const history = useHistory()
-    const {name, description, url, _id} = imageDetails
-
-    async function onClickDeleteImage () {
-        const authToken = localStorage.getItem('closetlyToken')
-            if (authToken) {
-        await axios.post("http://localhost:3000/removeImage", {imageId: _id} , {headers: {'Authorization': `Basic ${authToken}`}})
-            }else {
-                history.push('/login')
-            }
-    }
+    const {name, description, url} = imageDetails
     return (
         <div onClick={() => {
             setImageDetails(imageDetails)
             togglIsImageDetailsShown(true)}} className="image-card">
             <img className="image-photo" src={url} alt={name}/>
             <div className="photo-description">{description}</div>
-            <div className="delete-photo-icon" onClick={onClickDeleteImage}></div>
         </div>
     )
 }
--- src/components/Home/ImageDetails.tsx
+++ src/components/Home/ImageDetails.tsx
@@ -1,13 +1,41 @@
 import "./css/ImageDetails.css";
 import Modal from "../Utility/Modal";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
-function ImageDetails({togglIsImageDetailsShown}) {
+function ImageDetails({ toggleIsImageDetailsShown, selectedImageDetails, setUserImages }) {
+  console.log(selectedImageDetails);
+  const history = useHistory();
+  const { title, description, url, _id } = selectedImageDetails;
+
+  async function onClickDeleteImage() {
+    const authToken = localStorage.getItem("closetlyToken");
+    if (authToken) {
+      try {
+        await axios.post(
+          "http://localhost:3000/removeImage",
+          { imageId: _id },
+          { headers: { Authorization: `Basic ${authToken}` } }
+        );
+        setUserImages(state => state.filter(image => image._id !== _id))
+        toggleIsImageDetailsShown(false)
+      } catch (err) {
+        console.error("image could not be deleted", err)
+      }
+      
+    } else {
+      history.push("/login");
+    }
+  }
   return (
     <Modal>
-      <div className="buttons"></div>
-      <p className="image-name"></p>
-      <img className="clothing-image"></img>
-      <div className="additional-details"></div>
+      <div className="buttons">
+        <button onClick={() => toggleIsImageDetailsShown(false)}>X</button>
+        <div className="delete-photo-icon" onClick={onClickDeleteImage}></div>
+      </div>
+      <p className="image-name">{title}</p>
+      <img className="clothing-image" alt={`${title} - ${description}}`} src={url}></img>
+      <div className="additional-details">{description}</div>
     </Modal>
   );
 }
