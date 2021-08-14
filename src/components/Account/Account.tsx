faea29364f069b070b3907254f45e746e640f1f0
Nikita Sheremet <nikita.sheremet253@gmail.com>
2021-08-14 09:31:05 -0400

    add fields and logic for updating password

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,12 +1,40 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import {
-    Link
+    Link, useHistory
   } from "react-router-dom";
+  import axios from "axios";
 
 function Account() {
-  useEffect(() => {
-    console.log("ACCOUNT")
-})
+  const history = useHistory();
+  const [updatedPassword, setUpdatedPassword] = useState({pass1: "", pass2: ""})
+  const handleUpdatePassword = async () => {
+    const {pass1, pass2} = updatedPassword
+    if (pass1 === pass2) {
+      console.log("PASSWORDS MATCH")
+      const authToken = localStorage.getItem("closetlyToken");
+
+    if (authToken) {
+      try {
+        await axios.post(
+          "http://localhost:3000/updatePassword",
+          { newPassword: pass1},
+          { headers: { Authorization: `Basic ${authToken}` } }
+        );
+        console.log("Password was saved")
+        setUpdatedPassword({pass1: "", pass2: ""})
+      } catch (err) {
+        console.error("password could not be saved", err);
+      }
+    } else {
+      history.push("/login");
+    }
+    } else {
+      window.alert("Passwords do not match")
+    }
+  }
+  const handleNewPasswordOnChange = (e) => {
+    setUpdatedPassword(state => ({...state, [e.target.name]: e.target.value}))
+  }
  return (
   <div>
       <div>
@@ -23,7 +51,14 @@ function Account() {
             </li>
           </ul>
         </nav>
+
   </div>
+  <p>Update your password</p>
+  <label htmlFor="pass-1">Enter new password</label>
+  <input id="pass-1" name="pass1" onChange={handleNewPasswordOnChange} value={updatedPassword.pass1} type="password"></input>
+  <label htmlFor="pass-2">Re-enter password</label>
+  <input id="pass-2" name="pass2" onChange={handleNewPasswordOnChange} value={updatedPassword.pass2} type="password"></input>
+  <button id="pass-update" onClick={handleUpdatePassword}>Update Password</button>
   </div>
  )
 }
