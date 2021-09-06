c5f5b9af447102fd561a4766dc0bd5e972b8472d
Nikita Sheremet <nikita.sheremet253@gmail.com>
2021-09-06 16:31:28 -0400

    remove unused variable

--- src/components/Home/ImageUpload.tsx
+++ src/components/Home/ImageUpload.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import "./css/ImageUpload.css";
